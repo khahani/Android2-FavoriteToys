e087de04a1fde9faccd112dc7cf8014872e3c4f9
Mohammadreza Khahani <mr.khahani@gmail.com>
2018-07-19 15:21:58 -0700

    Practices

--- app/src/main/java/com/khahani/example/favoritetoys/MainActivity.java
+++ app/src/main/java/com/khahani/example/favoritetoys/MainActivity.java
@@ -3,6 +3,11 @@ package com.khahani.example.favoritetoys;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Developer and Teacher: Khahani.com
+ * Course: Android 2
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
